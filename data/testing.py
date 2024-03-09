adbe8edbe3f2b6343cae92e0ece896006f95fd2c
Sokol K <104557906+S0k0l-K@users.noreply.github.com>
2024-03-09 17:10:21 +0100

    Add files via upload
    Updated testing file

--- data/testing.py
+++ data/testing.py
@@ -1,11 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-test = np.load('X_test.npy')
-print(test.shape)
-from PIL import Image
+#Train data, rotated trial data and zoomed trial data loaded
+test = np.load('X_train.npy')
+rotated_trial = np.load('augmented_subset_images_trial.npy')
+zoomed_trial = np.load('zoomed_subset_images_trial.npy')
+# You can check the shape of the dataset from the commented line below
+# print(test.shape)
 
-image_to_display = np.squeeze(test[1])
+# from PIL import Image
 
-plt.imshow(image_to_display, cmap='gray')
-plt.show()
+# Code for displaying individual training images
+# image_to_display = np.squeeze(test[3])
+#
+# plt.imshow(image_to_display, cmap='gray')
+# plt.show()
+
+# Code for displaying individual rotated images
+# image_to_display = np.squeeze(rotated_trial[4])
+#
+# plt.imshow(image_to_display, cmap='gray')
+# plt.show()
+
+# Code for displaying individual zoomed images
+# image_to_display = np.squeeze(zoomed_trial[4])
+#
+# plt.imshow(image_to_display, cmap='gray')
+# plt.show()
+
 
