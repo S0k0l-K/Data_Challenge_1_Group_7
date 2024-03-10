74032f29c89161d312a55c3383da646a18edf571
Sokol Karabaxhak <s.karabaxhak@student.tue.nl>
2024-03-10 11:31:25 +0100

    Files submitted from the pycharm console directly, without the data because of git limit conflict.

---
+++ data/Data Augmentation.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+
+# Image generator of flipped and rotated imag
+
+x_train = np.load('X_train.npy')
+x_train = x_train.reshape((16841, 128, 128, 1))
+subset_images = x_train[:40]
+batch_size = 40
+
+data_generator = ImageDataGenerator(rotation_range=15)
+augmented_images_batch = next(data_generator.flow(subset_images, batch_size=batch_size, seed=3213))
+
+
+zoom_generator = ImageDataGenerator(zoom_range=0.1)#Zoom in or out by 10%
+x_train = np.load('X_train.npy')
+x_train = x_train.reshape((16841, 128, 128, 1))
+subset_images = x_train[:40]
+
+
+
+zoomed_images_batch = next(zoom_generator.flow(subset_images, batch_size=batch_size, seed=3213))
+
+np.save('zoomed_subset_images_trial.npy', zoomed_images_batch)
+np.save('augmented_subset_images_trial.npy', augmented_images_batch)
+
+
+
